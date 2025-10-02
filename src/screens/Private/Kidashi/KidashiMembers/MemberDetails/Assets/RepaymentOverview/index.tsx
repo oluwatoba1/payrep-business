32b3471e4fb00621db03fa3cfbd7aaff09c0bc73
Chosen2730 <patiencesimoniseoluwa@gmail.com>
2025-10-02 09:38:49 +0100

    added change

--- src/screens/Private/Kidashi/KidashiMembers/MemberDetails/Assets/RepaymentOverview/index.tsx
+++ src/screens/Private/Kidashi/KidashiMembers/MemberDetails/Assets/RepaymentOverview/index.tsx
@@ -69,6 +69,7 @@ const RepaymentOverview = ({
 	};
 
 	const getOtp = async () => {
+		console.log("====>>>", "getOtp", memberDetails);
 		try {
 			const { status, message } = await generateOtp({
 				purpose: "ASSET_REQUEST",
@@ -76,12 +77,14 @@ const RepaymentOverview = ({
 				subject_id: vendor?.id || "",
 				channel: "sms",
 			}).unwrap();
+			console.log("====>>>", "generateOtp", status, message);
 			if (status) {
 				showToast("success", "OTP has been sent to member");
 			} else {
 				showToast("danger", message);
 			}
 		} catch (error: ErrorResponse | any) {
+			console.log("====>>>", "error", error);
 			showToast(
 				"danger",
 				error.data?.message || error.message || DEFAULT_ERROR_MESSAGE
